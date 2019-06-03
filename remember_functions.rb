57f7b221da18881b3cadde78139ec3029e08d317
Rocco Nicholls <rocco11nicholls@gmail.com>
2019-06-03 10:45:42 -0600

    Updated Tools inspection

--- remember_functions.rb
+++ remember_functions.rb
@@ -1,11 +1,21 @@
-# Tools.random_color
-# Tools.count_each(arr)
-# Tools.months_between(date1, date2)
-# Tools.measure_block do ...
-# Tools.measure_x_blocks(count=1000) do ...
 
 class Tools
   class << self
+    def inspect
+      pst("Tools.random_color")
+      pst("Tools.count_each(arr)")
+      pst("Tools.months_between(date1, date2)")
+      pst("Tools.measure_block do ...")
+      pst("Tools.measure_x_blocks(count=1000) do ...")
+      pst("Tools.hashrocket_parse(\"{sup=>:foo}\")")
+
+      super
+    end
+
+    def pst(str)
+      puts "\e[33m#{str}\e[0m"
+    end
+
     # Random Color
     def random_color
       "##{('%06x' % (rand * 0xffffff))}".upcase
@@ -27,13 +37,13 @@ class Tools
     #=> 5
 
     # Measure how long a block of code takes
-    def single_measure(&block)
+    def measure_block(&block)
       t = Time.now.to_f
       yield
       Time.now.to_f - t
     end
-    def measure(count=10000, &block)
-      count.times.map { single_measure(&block) }.sum / count.to_f
+    def measure_x_blocks(count=10000, &block)
+      count.times.map { measure_block(&block) }.sum / count.to_f
     end
 
     def hashrocket_parse(raw_hashrocket_json)
